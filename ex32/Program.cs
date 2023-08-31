0fb4b9e40446812b5b699e7ca77f7489a807833f
LitavrinaAA <a.a.litavrina@google.com>
2023-08-31 22:10:09 +0300

    Change Neg Pos Array

---
+++ ex32/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 32: Напишите программу замена элементов
+//массива: положительные элементы замените на
+//соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+void ChangeNegPos(int[] array)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        
+            array[i] *= -1; 
+        
+    }
+}
+void PrintArray(int[] array){
+  int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {  
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+int[] array = { -4, -8, 8, 2 };
+PrintArray(array);
+ChangeNegPos(array);
+PrintArray(array);
+
